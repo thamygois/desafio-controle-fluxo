967c9a1b2cefc9d4facd2b0b12278cfb4454232f
Thamy Gois <thaamy_gois@hotmail.com>
2023-09-01 16:29:59 -0300

    Uptade: código pronto

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,18 +1,48 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Contador {
-    public static void main(String[] args) {
-        Scanner terminal  = new Scanner (System.in);
+    public static void main(String[] args) throws Exception {
+        Scanner terminal  = new Scanner (System.in).useLocale(Locale.US);
 
             System.out.println("Digite o primeiro parâmetro: ");
-                int parametroUm = terminal.next();
+                int parametroUm = terminal.nextInt();
 
             System.out.println("Digite o segundo parâmetro: ");
-                int parametroDois = terminal.next();
+                int parametroDois = terminal.nextInt();
 
-            try { contar (parametroUm, parametroDois);
+           terminal.close();
+
+       try { contar (parametroUm, parametroDois);
+        }
+
+        catch (ParametrosInvalidosException e) {
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro!" + e);
+        } 
+    }
+
+         public class ParametrosInvalidosException extends Exception {}
+
+        static void contar (int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+            
+           int contagem = 0;
+            if (parametroUm > parametroDois) {
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro!");;
+                
             }
 
-            
-    }
-}
+            else {
+                contagem = parametroDois - parametroUm;
+
+             int contagemInicial = 1;
+                for ( ; contagemInicial <= contagem; ) {
+                    System.out.println("Imprimindo número: " + contagemInicial);
+                    contagemInicial++;
+                } }
+       
+            }
+        
+       
+        
+    } 
+
